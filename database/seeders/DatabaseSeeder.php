96a4bce4c06f60469ab9b1117ffb96c5fb902ba5
arfani <arfanihidayat@gmail.com>
2024-01-12 08:39:43 +0800

    init 1.a & 1.c

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create(
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@mail.com',
+                'role' => 'admin',
+            ]
+        );
+
+
+        \App\Models\User::factory()->create(
+            [
+                'name' => 'user',
+                'username' => 'user',
+                'email' => 'user@mail.com',
+                'role' => 'user',
+            ]
+        );
+    }
+}
